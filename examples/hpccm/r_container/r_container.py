a1be8b5771a4d8e2a0bf52630ff0950fb3d69d0a
Geert Jan Bex <geertjan.bex@uhasselt.be>
2022-12-15 14:48:41 +0100

    Reorder recipe and add comments

--- examples/hpccm/r_container/r_container.py
+++ examples/hpccm/r_container/r_container.py
@@ -11,29 +11,29 @@ from pathlib import Path
 
 
 # Choose a base image
-Stage0.baseimage('ubuntu:21.04')
+Stage0.baseimage('ubuntu:22.04')
  
+# Install utilities
+Stage0 += apt_get(ospackages=['gist', 'wget', 'rclone', ])
+
 # Install build tools
 Stage0 += apt_get(ospackages=['build-essential', 'make'])
 
-# Install libraries
-Stage0 += apt_get(ospackages=['openssl', 'libxml2-dev', 'libcurl4-openssl-dev', 'libz-dev', 'libssl-dev',
-                              'libjpeg-dev', ])
 # Install GNU compilers (upstream)
 compilers = gnu()
 Stage0 += compilers
 
-# install GDAL library
-Stage0 += apt_get(ospackages=['libgdal-dev', 'libudunits2-dev'])
-
-# Install utilities
-Stage0 += apt_get(ospackages=['gist', 'wget', 'rclone', ])
-
+# Install libraries required to build R
+Stage0 += apt_get(ospackages=['openssl', 'libxml2-dev', 'libcurl4-openssl-dev', 'libz-dev', 'libssl-dev',
+                              'libjpeg-dev', ])
 # Install BLAS and Lapack
 Stage0 += apt_get(ospackages=['libopenblas-dev', 'liblapack-dev'])
 
 # Install R
 Stage0 += apt_get(ospackages=['r-base', ])
+
+# installing libraries required by R packages, e.g., GDAL library
+Stage0 += apt_get(ospackages=['libgdal-dev', 'libudunits2-dev'])
 
 # Install R packages
 Stage0 += copy(src='install_packages.R', dest='/setup/', _mkdir=True)
