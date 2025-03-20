32663fe731c2fae3643e2419620f0e0eead95bf1
Geert Jan Bex <geertjan.bex@uhasselt.be>
2025-03-20 14:22:39 +0100

    Update R ocntainer definition

--- examples/hpccm/r_container/README.md
+++ examples/hpccm/r_container/README.md
@@ -5,4 +5,6 @@ Defintion of an image to run R.
 ## What is it?
 
 1. `r_container.py`: hpccm definition of the image.
+1. `r_container.recipe`: Singularity/Apptainer definition of the image,
+   generated from `r_container.py` using hpccm.
 1. `install_packages.R`: R script to install additional libraries.
--- examples/hpccm/r_container/r_container.py
+++ examples/hpccm/r_container/r_container.py
@@ -13,36 +13,62 @@ Stage0.baseimage('ubuntu:22.04')
 Stage0 += apt_get(ospackages=['gist', 'wget', 'rclone', ])
 
 # Install tools to install R
-Stage0 += apt_get(ospackages=['software-properties-common', 'apt-transport-https', 'dirmngr', 'gnupg'])
+Stage0 += apt_get(ospackages=[
+    'software-properties-common',
+    'apt-transport-https',
+    'dirmngr',
+    'gnupg',
+])
 
 # Add signature key for CRAN
 Stage0 += shell(commands=['wget -qO- https://cloud.r-project.org/bin/linux/ubuntu/marutter_pubkey.asc | tee -a /etc/apt/trusted.gpg.d/cran_ubuntu_key.asc'])
 
-# Add CRAN repository
+# Add CRAN repository and signature key
 Stage0 += shell(commands=['add-apt-repository "deb https://cloud.r-project.org/bin/linux/ubuntu $(lsb_release -cs)-cran40/"'])
+Stage0 += shell(commands=['apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 51716619E084DAB9'])
 
-# Install build tools
-Stage0 += apt_get(ospackages=['build-essential', 'make'])
+# Install build tools and compilers
+Stage0 += apt_get(ospackages=[
+    'build-essential',
+    'make',
+])
 Stage0 += cmake(eula=True)
-
-# Install GNU compilers (upstream)
-compilers = gnu()
-Stage0 += compilers
+Stage0 += gnu()
 
 # Install HDF5 libraries
 Stage0 += hdf5(configure_opts=['--enable-cxx', '--enable-fortran',])
 
 # Install libraries required to build R
-Stage0 += apt_get(ospackages=['openssl', 'libxml2-dev', 'libcurl4-openssl-dev', 'libz-dev', 'libssl-dev',
-                              'libjpeg-dev', ])
+Stage0 += apt_get(ospackages=[
+    'openssl',
+    'libxml2-dev',
+    'libcurl4-openssl-dev',
+    'libz-dev', 'libssl-dev',
+    'libjpeg-dev',
+])
+
 # Install BLAS and Lapack
-Stage0 += apt_get(ospackages=['libopenblas-dev', 'liblapack-dev'])
+Stage0 += apt_get(ospackages=[
+    'libopenblas-dev',
+    'liblapack-dev',
+])
 
 # Install R
 Stage0 += apt_get(ospackages=['r-base', ])
 
-# installing libraries required by R packages, e.g., GDAL library
-Stage0 += apt_get(ospackages=['libgdal-dev', 'libudunits2-dev'])
+# Installing libraries required by R packages, e.g., GDAL library,
+# required for spatial data processing
+Stage0 += apt_get(ospackages=[
+    'libgdal-dev',
+    'libudunits2-dev',
+])
+
+# Install libraries required by tidyverse
+Stage0 += apt_get(ospackages=[
+    'libfontconfig1-dev',
+    'libharfbuzz-dev',
+    'libfribidi-dev',
+])
 
 # Install R packages
 Stage0 += copy(src='install_packages.R', dest='/setup/', _mkdir=True)
@@ -51,3 +77,6 @@ Stage0 += shell(commands=['Rscript /setup/install_packages.R'])
 # Set environment variables
 Stage0 += environment(variables={'LC_ALL': 'C.UTF-8'})
 Stage0 += environment(variables={'TZ': 'Europe/Brussels'})
+
+# Add runtime script
+Stage0 += runscript(commands=['R'])
