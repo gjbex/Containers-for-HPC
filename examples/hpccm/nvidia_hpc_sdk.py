b0cbf8c0fe6b73ff3ae260a8c64380908ba3991e
Geert Jan Bex <geertjan.bex@uhasselt.be>
2026-08-19 09:11:39 +0200

    Update NVIDIA HPC stack

--- examples/hpccm/nvidia_hpc_sdk.py
+++ examples/hpccm/nvidia_hpc_sdk.py
@@ -1,34 +1,36 @@
 '''Recipe to create either a docker container or Singularity image
-for a compute node on which users can log in using password authentication.
-A number of editors, git, make, CMake, GCC and Open-MPI
+for development with the NVIDIA HPC SDK.
+A number of utilities are also installed.
 
 Usage:
-    $ hpccm  --recipe development_cuda.py  --format docker
-    $ hpccm  --recipe development_cuda.py  --format singularity
+    $ hpccm  --recipe nvidia_hpc_sdk.py  --format docker
+    $ hpccm  --recipe nvidia_hpc_sdk.py  --format singularity
 '''
 
 from pathlib import Path
 
 # Choose a base image
-Stage0.baseimage('nvcr.io/nvidia/nvhpc:25.5-devel-cuda_multi-ubuntu22.04') 
+Stage0.baseimage('nvcr.io/nvidia/nvhpc:26.3-devel-cuda_multi-ubuntu22.04')
 
 # Install CMake
 Stage0 += cmake(eula=True)
 
 # Install editor and other tools
-Stage0 += apt_get(ospackages=['vim', 'less', 'ack', ])
-
-# Install archive and compression software and utitlies
-Stage0 += apt_get(ospackages=['tar', 'gzip', 'bzip2', 'wget', 'ca-certificates', ])
-
-# Install version control
-Stage0 += apt_get(ospackages=['git', 'openssh-client', ])
-
-# Install debugging tools
-Stage0 += apt_get(ospackages=['valgrind', 'strace', ])
-
-# Install benchmarking tools
-Stage0 += apt_get(ospackages=['hyperfine'])
+Stage0 += apt_get(ospackages=[
+    'vim',
+    'less',
+    'ack',
+    'tar',
+    'gzip',
+    'bzip2',
+    'wget',
+    'ca-certificates', 
+    'git',
+    'openssh-client',
+    'valgrind',
+    'strace',
+    'hyperfine',
+])
 
 # add run script, i.e., start bash
 Stage0 += runscript(commands=['/bin/bash'])
